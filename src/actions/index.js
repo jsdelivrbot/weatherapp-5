b4097b34f97760db604b1fbd7885d88b40df64df
Kris Pinter <Kris.Pinter@hq.peninsula-uk.local>
2017-01-24 17:23:33 +0000

    wip

---
+++ src/actions/index.js
@@ -0,0 +1,17 @@
+import axios from 'axios';
+
+//const API_KEY = '00e61a1a957f7d58c46c21ac625c2603';
+const API_KEY = '6a78596d062df78380eff5944c4e5567';
+const ROOT_URL = `http://api.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`;
+
+export const FETCH_WEATHER = 'FETCH_WEATHER';
+
+export function fetchWeather(city) {
+  const url = `${ROOT_URL}&q=${city},uk`;
+  const request = axios.get(url);
+
+  return {
+    type: FETCH_WEATHER,
+    payload: request
+  };
+}
